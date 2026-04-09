9e970515d5bfc7bda6c77ab2ed0abfecd9940c8a
EllinaRoman <evelinka.late@yandex.ru>
2026-04-09 14:30:21 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,21 @@
+const dropdowns = Array.from(document.querySelectorAll('.dropdown'));
+
+dropdowns.forEach((dropdown) => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const items = Array.from(dropdown.querySelectorAll('.dropdown__item'));
+
+    value.addEventListener('click', () => {
+    list.classList.toggle('dropdown__list_active');
+})
+
+items.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        value.textContent = item.textContent;
+        list.classList.toggle('dropdown__list_active');
+    })
+})
+})
+
+
